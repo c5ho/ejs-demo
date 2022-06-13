e354ae92ca4cf78e13eab47d68aec3ffcc48dc31
Calvin Ho <calvin.ch.ho@gmail.com>
2022-06-13 18:05:49 -0400

    Rendered a variable in EJS to pass between views and partials

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
+// use res.render to load up an ejs view file, from the views folder
 
-// index page
+// index page (full path is views/pages/index)
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+  
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  }); 
 });
 
 // about page
